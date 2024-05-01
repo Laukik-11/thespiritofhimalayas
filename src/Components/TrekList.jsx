172df1b301f2d768723284e78683d9f65baf1a67
laurel verma <56593513+1laurelverma@users.noreply.github.com>
2024-05-01 17:08:15 +0530

    updated trek description

--- src/Components/TrekList.jsx
+++ src/Components/TrekList.jsx
@@ -3,9 +3,16 @@ import { Link } from "react-router-dom";
 import Dodham from "../Assets/Dodham.jpg";
 import Chardham from "../Assets/Chardham.jpg";
 import Badrinath from "../Assets/Badrinath.jpg";
-import Sunderdunga from "../Assets/Sunderdunga.jpg";
-import Kedarnath from "../Assets/Kedarnath.jpg";
-import Tungnath from "../Assets/Tungnath.jpg";
+// import Sunderdunga from "../Assets/Sunderdunga.jpg";
+import Kedarnath from "../Assets/kedarnath1.jpg";
+import Tungnath1 from "../Assets/tungnath1.jpeg";
+import Sunderdunga1 from "../Assets/sundardunga1.jpeg";
+import Kafini1 from "../Assets/kafini1.jpeg";
+import Katarmal1 from "../Assets/katarmal1.jpeg";
+import Kausani1 from "../Assets/kausani1.jpeg";
+import Dolashram1 from "../Assets/dolashram1.jpeg";
+import Pindari1 from "../Assets/pindari1.jpeg";
+import Pindari2 from "../Assets/pindari2.jpeg";
 
 export const Tours = [
   {
@@ -43,75 +50,108 @@ export const treks = [
   {
     id: 1,
     trekname: "Pindari",
-    image: Dodham,
+    image: Pindari1,
     description: [
+      "Area:   Kumaon HimalayaTrek ",
+      "Duration:   10 Days (4 days traveling/6 days trekking) ",
+      "Season:   March - June and September - December (Except July & August) ",
+      "Trek SeasonGrade:   ModerateAltitude ",
+      "Altitude:   4150 mts ",
+      "Total Trekking:   90 K.M ",
       "The Pindari Glacier is a glacier found in the upper reaches of the Kumaon Himalayas, to the southeast of Nanda Devi and Nanda Kot.",
       "The glacier is about 9 kilometers long [1] and gives rise to the Pindar River which meets the Alakananda at Karnaprayag in the Garhwal district.",
       "The trail to reach the glacier crosses the villages of Saung, Loharkhet, crosses over the Dhakuri Pass, continues onto Khati village (the last inhabited village on the trail), Dwali, Phurkia and finally Zero Point, Pindar, the end of the trail. Though most of the trail is along the banks of the Pindari River, the river is mostly hidden until after Khati.",
       "The Pindari Glacier trail provides for a 90 km (56 mi) round-trip trek that most people find comfortable to complete in 6 days. The Pindari Glacier is also famous for other adventure sports like Ice climbing and Mountain biking.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
   {
     id: 2,
     trekname: "Kafini",
-    image: Chardham,
+    image: Kafini1,
     description: [
+      "Region: Kumaon ",
+      "Total Trekking: 70 Km ",
+      "Grade: Moderate ",
+      "Altitude: 3895 Mt ",
+      "Duration: 8Days/Night ",
+      "Temperature: May — June (15C to 20C and night 7C to2C) Sept — Oct (15C to 10C and night 5C to -5C).",
+      "Best Season:  Pre Monsoon Season:— May first week to June end and post monsoon Season:- September 2nd week to Nov first week.",
       "The Kafni Glacier is located in the upper reaches of the Kumaon Himalayas, to the southeast of Nanda Devi. The glacier gives rise to the Kafni River, which is a tributary of the Pindar River. The Pindar River is a tributary to Alaknanda River, which eventually is one of the two headstreams of the Ganges. This is relatively small glacier but a popular trekking destination along with Pindari Glacier",
-      "Temperature : May — June (15C to 20C and night 7C to2C) Sept — Oct (15C to 10C and night 5C to -5C).",
-      "Best Season Pre Monsoon Season:— May first week to June end and post monsoon Season:- September 2nd week to Nov first week.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
   {
     id: 3,
-    trekname: "Sunderdunga",
-    image: Sunderdunga,
+    trekname: "Sunder dunga",
+    image: Sunderdunga1,
     description: [
-      "Sunderdhunga Glacier is located in the Bageshwar district of Uttarakhand. Adorned with an intriguing panorama, this glacier sits close to Sukhram and Maiktoli Glacier. Sunderdhunga trek is a gripping one as it opens up to spectacular views of imposing peaks like Maiktoli, Mrigthuni, Tharkot and Panwalidwar. A paradise for nature lovers and keen photographers, Sunderdhunga Glacier is a must see natural wonder. Sunderdhunga trek is one of the best trek of uttarakhand.",
-      "Sunderdhunga Valley trek is one of the most beautiful trek in Uttrakhand. This trek used to start from Kharkiya, Jatoli used to be its first camp, The Valley of Beautiful Stones.Offering great views of Maiktoli peak, Mrigthuini, Tharkot and Panwali Dwar a very beautiful one indeed ! New Jeep road has opened up till Kharkiya which skips the campsite and saves a day. Although, if you are trekking to experience the Himalayas and its beauty, This trail follows the same route as that of the adjacent glaciers of Pindari and Kafni, before separating at the village of Khati. The trek takes you through dense forest, some of the remote villages of Kumaon and then to the ravine of the Sunderdhunga river to the beautiful campsite of Kathaliya, which then acts as your Exploratory base camp of the beautiful valley. Here you are surrounded by some of the most beautiful peaks in the region. Trek upto Maiktoli base camp and then to the Balooni top, Sukhram caves offers a spectacular view of the surrounding, something which will remain with your forever.Devi kund, is a small hike from Balooni Top If trekker knows about the place to some extent, he would know that even the name expresses the place to some extent, the magnificence of the scenery if far beyond the word ‘sunder (which means beautiful)’ and ‘dhunga’ which means ‘valley of stones’) can express together. In all its harmony, every single elements of Nature stands out to express its presence there (even fire when camps are set up).The point that goes to Pindari is from where the journey to Sunderdhunga also starts,The trek Sunderdhunga glacier is best enjoyed in the months Apr to Jun and Sep to Oct which generally takes 8 to 10 days. ",
-      "Destinations: Bageshwar, Saung, Loharkhet, Khati, Jatoli, Kathalaya, Sunderdhunga",
-      "Experience: Trekking (moderate to challenging)",
-      "Maximum Altitude: 4,320 m (Approx)",
-      "Duration: 8 days",
-      "Ideal Time: May, June, September and October",
+      "Destinations: Bageshwar, Saung, Loharkhet, Khati, Jatoli, Kathalaya, Sunderdhunga ",
+      "Experience: Trekking (moderate to challenging) ",
+      "Maximum Altitude: 4,320 m (Approx) ",
+      "Duration: 8 days ",
+      "Ideal Time:  May, June, September and October ",
+      "Sunderdhunga Glacier is located in the Bageshwar district of Uttarakhand. Adorned with an intriguing panorama, this glacier sits close to Sukhram and Maiktoli Glacier. Sunderdhunga trek is a gripping one as it opens up to spectacular views of imposing peaks like Maiktoli, Mrigthuni, Tharkot and Panwalidwar. A paradise for nature lovers and keen photographers, Sunderdhunga Glacier is a must see natural wonder. Sunderdhunga trek is one of the best trek of uttarakhand.",
+      "Sunderdhunga Valley trek is one of the most beautiful trek in Uttrakhand. This trek used to start from Kharkiya, Jatoli used to be its first camp, The Valley of Beautiful Stones.Offering great views of Maiktoli peak, Mrigthuini, Tharkot and Panwali Dwar a very beautiful one indeed ! New Jeep road has opened up till Kharkiya which skips the campsite and saves a day. Although, if you are trekking to experience the Himalayas and its beauty, This trail follows the same route as that of the adjacent glaciers of Pindari and Kafni, before separating at the village of Khati. The trek takes you through dense forest, some of the remote villages of Kumaon and then to the ravine of the Sunderdhunga river to the beautiful campsite of Kathaliya, which then acts as your Exploratory base camp of the beautiful valley. Here you are surrounded by some of the most beautiful peaks in the region. Trek upto Maiktoli base camp and then to the Balooni top, Sukhram caves offers a spectacular view of the surrounding, something which will remain with your forever.Devi kund, is a small hike from Balooni Top If trekker knows about the place to some extent, he would know that even the name expresses the place to some extent, the magnificence of the scenery if far beyond the word ‘sunder (which means beautiful)’ and ‘dhunga’ which means ‘valley of stones’) can express together. In all its harmony, every single elements of Nature stands out to express its presence there (even fire when camps are set up).The point that goes to Pindari is from where the journey to Sunderdhunga also starts,The trek Sunderdhunga glacier is best enjoyed in the months Apr to Jun and Sep to Oct which generally takes 8 to 10 days.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
   {
     id: 4,
     trekname: "Katarmal sun temple",
-    image: Kedarnath,
+    image: Katarmal1,
     description: [
-      "At a distance of 30 km from Dwarahat and 20 km from Almora, Katarmal Sun Temple is an ancient Hindu temple situated at Katarmal village in Almora district of Uttarakhand.Katarmal Temple is dedicated to Lord Burhadita or Vriddhaditya (the old Sun God) and is one of the famous historical places of Kumaon hills in Uttarakhand.",
-      "If you are planning to undertake this journey, it is important to prepare yourself well in advance to ensure a smooth and successful trip. So to have a tension free trip pan your journey with us.The present image of Surya in the temple dates back to 12th century. Built in the 9th century by Katarmalla, a Katyuri king, this temple is perched at an altitude of 2116 m.The Sun Temple is noted for its magnificent architecture and is a monument of historical importance.  The idols of Shiva-Parvati and Lakshmi-Narayana can also be spotted in this temple. The building faces the east in a way such that the first rays of the sun fall on the Shiva Lingam. Stone-built walls, intricate figures, beautiful pillar carvings and exquisitely cut out wooden doors are among the distinctive features of this temple. ",
+      "place: DWARAHAT ",
+      "weather: 7 °C-21°C ",
+      "Altitude: 4954.068 FT ",
+      "LANGUAGE: HINDI,KUMAONI ",
+      "Ideal Time: APRIL TO JUNE,SEPT TO NOV ",
+      "RLY STATION: KATHGODAM ",
+      "AIRPORT: PANTNAGAR ",
       "The Katarmal Surya Temple is a 9th-century temple built by Katarmalla, a Katyuri King, exhibiting the architectural style of the ancient artisans. This Hindu temple is perched at an elevation of 2,116 meters above sea level and is situated at a distance of 19 kms from Almora. ",
-      "This Surya Mandir (Sun Temple) is also known as Badaditya (Baraditya) Temple.",
+      "This Surya Mandir (Sun Temple) is also known as Badaditya (Baraditya) Temple.",
+      "Katarmal Sun Temple, set in the lap of the mighty Himalayas, is a well-kept secret of Almora in Uttarakhand. The temple is dedicated to the Sun God and is a splendid amalgam of stone carvings and religious belief. The mysterious temple has a deep connection to the cosmos that will leave you intrigued.",
+      "The quaint village of Katarmal is only 16 km away from the popular hill station of Almora. Let’s find out more about the mysteries that surround this ancient temple.",
+      "At a distance of 30 km from Dwarahat and 20 km from Almora, Katarmal Sun Temple is an ancient Hindu temple situated at Katarmal village in Almora district of Uttarakhand.Katarmal Temple is dedicated to Lord Burhadita or Vriddhaditya (the old Sun God) and is one of the famous historical places of Kumaon hills in Uttarakhand. The present image of Surya in the temple dates back to 12th century. Built in the 9th century by Katarmalla, a Katyuri king, this temple is perched at an altitude of 2116 m.The Sun Temple is noted for its magnificent architecture and is a monument of historical importance.  The idols of Shiva-Parvati and Lakshmi-Narayana can also be spotted in this temple. The building faces the east in a way such that the first rays of the sun fall on the Shiva Lingam. Stone-built walls, intricate figures, beautiful pillar carvings and exquisitely cut out wooden doors are among the distinctive features of this temple.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
   {
     id: 5,
     trekname: "Dol ashram",
-    image: Kedarnath,
+    image: Dolashram1,
     description: [
       "Shri Kalyanika Himalaya Devasthanam, popularly known as the Dol Ashram is a peaceful ashram in the quaint village of Kanara (Dol), located in the Almora district of Uttarakhand. Dol Ashram is surrounded by dense forests and rests calmly at its center.",
       "Dol Ashram is built for yoga and meditation purposes and the atmosphere of the ashram makes it an ideal location for practicing the elements of a spiritual life. At Dol Ashram, you can learn Vedic methodology which is an integral aspect for living a balanced and healthy life. Dol Ashram welcomes everyone who want to learn about spirituality, yoga, and meditation.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
   {
     id: 6,
     trekname: "Tungnath",
-    image: Tungnath,
+    image: Tungnath1,
     description: [
+      "Location:	Rudraprayag ",
+      "Starting Point: Chopta ",
+      "Famous For:	Temple, Panch Kedar (it’s a part of Third kedar ), Trekking, Bird Watching ",
+      "Altitude :	12,073 feet ",
+      "Total Trekking :	3.5 Km Trek From Chopta ", 
+      "Nearest Railway / Airport	Rishikesh: 230kms/ Jolly Grant Airport, 240 kms ",
+      "Best time to visit Ukhimath: May, Jun, Jul, Aug, Sep, Oct, Nov, ",
       "Tungnath, serenely nestled on Chandranath Parvat, stands as the world’s highest Shiva temple and the tallest among the Panch Kedar temples in Uttarakhand.  Tungnath is the third temple in the Panch Kedar pilgrimage sequence, Temple is situated at an impressive altitude of 3,680 meters above sea level,  The temple is believed to be more than 1,000 years old, carrying a rich historical and cultural heritage",
       "The Tungnath Temple, standing tall with its magnificent architecture, has a history spanning over a thousand years. Dedicated to Lord Shiva, you’ll also find idols of Goddess Parvati and other gods nearby. Adi Shankaracharya discovered this sacred temple, and today, it is tended to by local Brahmins from Makku village. ",
       "During winters, the symbolic image of Lord Shiva is shifted to Mukunath, which is 19 km away. The trek to Tungnath takes you through rocky terrains, lush meadows, and rhododendron bushes, treating you to stunning views of nature. The sight of the grand Himalayas along the way uplifts the spirits of trekkers. Trek offers clear views of numerous named and unnamed peaks of the majestic Himalayan range.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
   {
     id: 7,
     trekname: "Kausani ",
-    image: Kedarnath,
+    image: Kausani1,
     description: [
       "Kausani (Kumaoni: Kôsānī) is a hill station and Village situated in Bageshwar district in the state of Uttarakhand, India. It is famous for its scenic splendour and its spectacular 300 km-wide panoramic view of Himalayan peaks like Trisul, Nanda Devi and Panchchuli. Mahatma Gandhi called this place the 'Switzerland of India',[4][5] due to similarity in landscapes.",
       "Anashakti Ashram is a quiet and revered place where Mahatma Gandhi spent some days and wrote his commentary of Anashakti Yog.[21] The KMVN (Kumaon Mandal Vikas Nigam) resthouse in Kausani is an ideal place to stay and it provides panoramic views of the mountains.",
     ],
+    imageCarousal: [Pindari1, Pindari2],
   },
 ];
--- src/Pages/TrekDetail.jsx
+++ src/Pages/TrekDetail.jsx
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { treks, Tours } from "../Components/TrekList";
 import emailjs from "@emailjs/browser";
+import Slider from "../Components/Slider";
 
 const TrekDetail = () => {
   const { tourortrek, id } = useParams();
   // console.log("tour", tourortrek, id);
   const [trek, setTrek] = useState(null);
   useEffect(() => {
-    if(tourortrek === 'tour')
-    {
+    if (tourortrek === "tour") {
       const tour = Tours.find((trek) => trek.id === parseInt(id));
       setTrek(tour);
-    }
-    else if(tourortrek === 'trek')
-    {
+    } else if (tourortrek === "trek") {
       const trek = treks.find((trek) => trek.id === parseInt(id));
       setTrek(trek);
     }
-  }, [id,tourortrek]);
+  }, [id, tourortrek]);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -62,106 +60,113 @@ const TrekDetail = () => {
     return <div>Loading...</div>;
   }
   return (
-    <main class="app">
-      <div class="app__sections_left">
-        <div className="projects__image">
-          <img
-            src={trek.image}
-            alt="Image"
-            style={{ maxHeight: "500px", objectFit: "contain", width: "100%" }}
-          />
+    <>
+      <main class="app" style={{ padding: "20px 80px 60px 80px" }}>
+        <div class="app__sections_left">
+          <div className="projects__image">
+            <img
+              src={trek.image}
+              alt="Image"
+              style={{
+                maxHeight: "500px",
+                objectFit: "contain",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div className="app__heading__container">
+            <i class="fa fa-map-marker" style={{ fontSize: {} }}></i>
+            <h1 class="app__title">{trek.trekname}</h1>
+          </div>
+          {/* <p class="app__text">{trek.description}</p> */}
+          <ul className="treklist-ul">
+            {trek.description.map((trekpoint, index) => {
+              return (
+                <li key={index} className="treklist-li">
+                  {trekpoint}
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        <div className="app__heading__container">
-        <i class="fa fa-map-marker" style={{ fontSize: {}}}></i> 
-        <h1 class="app__title">{trek.trekname}</h1>
-        </div>
-        {/* <p class="app__text">{trek.description}</p> */}
-        <ul className="treklist-ul">
-          {trek.description.map((trekpoint, index) => {
-            return <li key={index} className="treklist-li">
-              {trekpoint}
-            </li>;
-          })}
-        </ul>
-      </div>
-      <div class="app__sections_right" style={{ maxWidth: "500px" }}>
-        <div class="app__form__heading">Book Now!!!</div>
-        <form onSubmit={handleSubmit} class="app__form" id="form">
-          {/* <form class="app__form" id="form"> */}
-          <div class="app__input__wrapper">
-            <input
-              required
-              className="app__form__fields"
-              type="text"
-              placeholder="Full Name*"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <span class="app__form__input__erro">
-              your name must be between 2 and 100 characters and only contain
-              letters
-            </span>
-          </div>
-          <div class="app__input__wrapper">
-            <input
-              required
-              class="app__form__fields"
-              type="email"
-              placeholder="Email-Id*"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <span class="app__form__input__erro">
-              Email address must be valid
-            </span>
-          </div>
-          <div class="app__input__wrapper">
-            <input
-              class="app__form__fields"
-              type="tel"
-              placeholder="Phone-Number"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-            <span class="app__form__input__erro">
-              Phone number must be 10 digit
-            </span>
-          </div>
-          <div class="app__input__wrapper">
-            <input
-              class="app__form__fields"
-              type="number"
-              placeholder="People"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-            />
-            <span class="app__form__input__erro">Number of people</span>
-          </div>
-          <div class="app__input__wrapper">
-            <textarea
-              required
-              class="app__form__fields"
-              cols="30"
-              rows="10"
-              placeholder="Description*"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
+        <div class="app__sections_right" style={{ maxWidth: "500px" }}>
+          <div class="app__form__heading">Book Now!!!</div>
+          <form onSubmit={handleSubmit} class="app__form" id="form">
+            {/* <form class="app__form" id="form"> */}
+            <div class="app__input__wrapper">
+              <input
+                required
+                className="app__form__fields"
+                type="text"
+                placeholder="Full Name*"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <span class="app__form__input__erro">
+                your name must be between 2 and 100 characters and only contain
+                letters
+              </span>
+            </div>
+            <div class="app__input__wrapper">
+              <input
+                required
+                class="app__form__fields"
+                type="email"
+                placeholder="Email-Id*"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span class="app__form__input__erro">
+                Email address must be valid
+              </span>
+            </div>
+            <div class="app__input__wrapper">
+              <input
+                class="app__form__fields"
+                type="tel"
+                placeholder="Phone-Number"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+              <span class="app__form__input__erro">
+                Phone number must be 10 digit
+              </span>
+            </div>
+            <div class="app__input__wrapper">
+              <input
+                class="app__form__fields"
+                type="number"
+                placeholder="People"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+              />
+              <span class="app__form__input__erro">Number of people</span>
+            </div>
+            <div class="app__input__wrapper">
+              <textarea
+                required
+                class="app__form__fields"
+                cols="30"
+                rows="10"
+                placeholder="Description*"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
 
-            <span class="app__form__input__erro">Add some discription</span>
-          </div>
-          {/* <button type="submit"></button> */}
+              <span class="app__form__input__erro">Add some discription</span>
+            </div>
+            {/* <button type="submit"></button> */}
 
-          <button type="submit" class="app__form__button">
-            Send Email
-          </button>
-          {/* <p class="app__form__term">
+            <button type="submit" class="app__form__button">
+              Send Email
+            </button>
+            {/* <p class="app__form__term">
             By clicking the button, you are agreeing to our{" "}
             <a href="#">Terms and Services</a>
           </p>  */}
-        </form>
+          </form>
 
-        {/* <form onSubmit={handleSubmit} className="emailForm">
+          {/* <form onSubmit={handleSubmit} className="emailForm">
           <input
             type="text"
             placeholder="Your Name"
@@ -182,8 +187,10 @@ const TrekDetail = () => {
           ></textarea>
           <button type="submit">Send Email</button>
         </form> */}
-      </div>
-    </main>
+        </div>
+      </main>
+      <Slider imagescarousal = {trek.imageCarousal}/>
+    </>
   );
 };
 
